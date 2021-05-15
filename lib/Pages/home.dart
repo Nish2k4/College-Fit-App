b5570a26320c25506f791ff379c0998c5f8399e9
Nishthha <nishthhapandya@gmail.com>
2021-05-15 15:51:46 -0400

    Home Page
    Finished designing the home page and added the search button.

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -9,7 +9,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlue [600],
+      backgroundColor: Colors.indigo [600],
       appBar: AppBar(
         backgroundColor: Colors.yellow [800],
         title: Row(
@@ -17,7 +17,7 @@ class _HomeState extends State<Home> {
           children: [
             Text("College Fit "),
             Image.asset(
-              "Images/Logo.png",
+              "Images/FinalLogo.png",
               fit: BoxFit.contain,
               height: 32,
             ),
@@ -37,9 +37,83 @@ class _HomeState extends State<Home> {
                     style: TextStyle(
                       fontSize: 25,
                       color: Colors.amber,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ]
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 15, 0, 20), // Space between title and features
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget> [
+                    Text(
+                      "College Tours\nPrerequesites\nTop 5 Majors\nGraduation Rate\nTransportation\nStudent Living\nEating Options\nWeather\nCollege Tuition", // List of features
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.amber,
+                        height: 1.5,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    border: OutlineInputBorder(),
+                    hintText: "Search",
+                    fillColor: Colors.white,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget> [
+                  Container( // Create a Search button
+                    width: 160,
+                    height: 47,
+                    margin: EdgeInsets.only(top: 30),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      // Add shadow behind the button
+                      boxShadow: <BoxShadow> [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 1,
+                          offset: Offset(10, 10),
+                        ),
+                      ],
+                    ),
+                    child: RaisedButton(
+                      onPressed: (){},
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+                      padding: EdgeInsets.all(0),
+                      child: Ink(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [Color(0xff374ABE), Color(0xff64B6FF)],
+                            begin: Alignment.topLeft,
+                            end: Alignment.topRight,
+                          ),
+                          borderRadius: BorderRadius.circular(47)),
+                        child: Container(
+                          constraints: BoxConstraints(maxWidth: 300, maxHeight: 50),
+                          alignment: Alignment.center, //Text placement in the button
+                          child: Text(
+                            "Search",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
